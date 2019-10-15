f50b5fa1873a3de0b90d94027bb711d2be5da612
alxtver <alxtver@gmail.com>
2019-10-15 20:21:58 +0300

    qq

--- routes/apkzi.js
+++ routes/apkzi.js
@@ -5,18 +5,13 @@ const router = Router()
 
 
 router.get('/', auth, async (req, res) => {
-    
     Apkzi.find().distinct('part', function(error, part) {
-        console.log(part)
         res.render('apkzi', {
             title: 'АПКЗИ',
             isApkzi: true,
             part: part
           })
-    });
-    
-    
-    
+    })
   })
 
 module.exports = router
