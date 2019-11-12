e1572919bf4c3ef5d52fd4a79a780a21551ad79e
alxtver <alxtver@gmail.com>
2019-11-12 17:25:23 +0300

    newYear

--- routes/home.js
+++ routes/home.js
@@ -8,13 +8,19 @@ const APKZI = require('../models/apkzi')
 router.get('/', auth, async (req, res) => {
   countPKI = await PKI.countDocuments()
   countPC = await PC.countDocuments()
-  
+  let dateNow = new Date()  
+  let newYear = Date.parse(dateNow.getFullYear())  
+  countPCinYear = await PC.find({created: {$gt: newYear}}).count()
 
+
+  
+  
   res.render('index', {
     title: 'Главная страница',
     isHome: true,
     countPKI: countPKI,
-    countPC: countPC
+    countPC: countPC,
+    countPCinYear: countPCinYear
   })
 })
 
