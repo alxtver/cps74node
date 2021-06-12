7e9d464635b45e6ce535309b21c9c70d351c99c7
alxtver <alxtver@gmail.com>
2021-06-12 23:37:56 +0300

    работа со сборкой системного блока

--- public/app.js
+++ public/app.js
@@ -271,7 +271,7 @@ function autocomplete(inp, arr) {
   });
 
   inp.addEventListener("keydown", function (e) {
-    var x = document.getElementById(this.id + "autocomplete-list");
+    const x = document.getElementById(this.id + "autocomplete-list");
     if (x) x = x.getElementsByTagName("div");
     if (e.key === "ArrowDown") {
       currentFocus++;
@@ -801,5 +801,183 @@ function painting() {
   }
   for (const cell of snCells) {
     cell.style.backgroundColor = cell.innerHTML === "" ? "darkgray" : "";
+  }
+}
+
+function createSystemCaseTable(systemCase, container, assemblyTable = false) {
+  const table = document.createElement("table");
+  table.className = "table table-sm table-bordered table-hover";
+  if (assemblyTable) {
+    table.className = 'table table-bordered table-hover assemblytable'
+  }
+  table.id = systemCase._id;
+  let row = table.insertRow();
+  let columnNames = [
+    "Обозначение изделия",
+    "Наименование изделия",
+    "Характеристика",
+    "Количество",
+    "Заводской номер",
+    "Примечания",
+  ];
+  if (assemblyTable) {
+    columnNames = [
+      "Наименование изделия",
+      "Характеристика",
+      "Количество",
+      "Заводской номер",
+    ];
+  }
+
+  // шапка
+  if (!assemblyTable) {
+    insCell("", row, "ФДШИ." + systemCase.fdsi, "up", "", false);
+  }
+  let cell = document.createElement("td");
+  cell.innerHTML = systemCase.serialNumber;
+  cell.id = assemblyTable ? 'serial_number' : systemCase.serialNumber;
+  cell.style.cssText =
+    "font-size: 1.5rem;background-color:" + systemCase.back_color;
+  row.appendChild(cell);
+  insCell("", row, systemCase.arm, "up", "", false);
+  insCell("", row, systemCase.execution, "up", "", false);
+  if (!assemblyTable) {
+    insCell("", row, "", "up", "", false);
+  }
+
+  cell = document.createElement("td");
+  cell.innerHTML = systemCase.attachment;
+  cell.style.cssText =
+    "font-size: 1.1rem;border-radius: 0px 10px 0px 0px;background-color:" +
+    systemCase.back_color;
+  row.appendChild(cell);
+  row = table.insertRow();
+  for (const name of columnNames) {
+    insCell("", row, name, "table-dark", "", false);
+  }
+  for (const unit of systemCase.systemCaseUnits) {
+    const row = table.insertRow();
+    row.className = 'system-case-row'
+    if (!assemblyTable) {
+      insCell("", row, unit.fdsi, "", "", false, { id: systemCase._id });
+    }
+
+    insCell("", row, unit.type, "type", "", false, { id: systemCase._id });
+    insCell("", row, unit.name, "name", "", false, { id: systemCase._id });
+    insCell("", row, unit.quantity, "", "", false, { id: systemCase._id });
+    const serialNumberCell = row.insertCell();
+    serialNumberCell.innerHTML = unit.serial_number;
+    serialNumberCell.dataset.id = systemCase._id;
+    serialNumberCell.dataset.obj = unit.i;
+    serialNumberCell.dataset.unit = "system_case_unit";
+    serialNumberCell.contentEditable = "true";
+    if (unit.szi) {
+      serialNumberCell.dataset.apkzi = "szi";
+    }
+    serialNumberCell.dataset.data = systemCase._id + ";" + unit.i + ";" + "system_case_unit";
+    serialNumberCell.className = "serial_number";
+    serialNumberCell.addEventListener("keypress", function (e) {
+      if (e.key === "Enter") {
+        e.preventDefault();
+        const id = e.target.dataset.id;
+        const obj = e.target.dataset.obj;
+        const unit = e.target.dataset.unit;
+        const serial_number = e.target.innerText;
+        const data_hidd = e.target.dataset.data;
+        const data_apkzi = e.target.dataset.apkzi;
+        document.getElementById("hidd_id").value = data_hidd;
+        if (data_apkzi) {
+          edit_serial_number_apkzi(id, obj, unit, serial_number);
+        } else {
+          editSerialNumber(id, obj, unit, serial_number);
+        }
+      }
+    });
+    if (!assemblyTable) {
+      insCell("", row, unit.notes, "", "", false, { id: systemCase._id });
+    }
+  }
+
+  const div = document.createElement("div");
+  div.id = systemCase._id;
+  div.className = "pcCard mb-3 table-responsive";
+  div.style.cssText =
+    "-webkit-box-shadow: 0 30px 60px 0" +
+    systemCase.back_color +
+    ";box-shadow: 0 30px 60px 0" +
+    systemCase.back_color;
+  container.appendChild(div);
+  div.innerHTML = "";
+  div.appendChild(table);
+
+  buttons(div, systemCase, `/systemCases/${systemCase._id}/edit?allow=true`)
+}
+
+function editSerialNumber(id, obj, unit, serialNumber) {
+  serialNumber = translate(serialNumber).ruToEnSN
+  const data = {
+    id: id,
+    obj: obj,
+    unit: unit,
+    serialNumber: serialNumber
+  }
+  putData('/systemCases/editSerialNumber', data).then((data) => {
+    const serialNumber = data.systemCase?.serialNumber || null
+    flashAlert(data, serialNumber)
+    if (!data.duplicatePki) {
+      const oldSystemCase = (data.oldSystemCase !== serialNumber) ? data.oldSystemCase : null
+      updateCells(data.systemCase, oldSystemCase)
+      const id = data.systemCase._id
+      socket.emit('updateAssemblyPC', {
+        serialNumber,
+        id
+      })
+    }
+  })
+}
+
+function updateCells(systemCase, oldSystemCase, voice = true) {
+  if (oldSystemCase) {
+    refreshOneTable(oldSystemCase)
+  }
+  refreshOneTable(systemCase)
+
+  //переход на одну ячейку вниз
+  let currentId = document.getElementById('hidd_id').value
+  let nextId = currentId.split(";")
+  nextId[1] = Number(nextId[1]) + 1 + ''
+  if (!document.querySelector('.popup-checkbox').checked) {
+    let nextCell = document.querySelector(".serial_number[data-data='" + nextId.join(';') + "']")
+    let nextCellText
+    if (nextCell) {
+      nextCellText = nextCell.innerHTML
+      while (/[Бб].?[Нн]/g.test(nextCellText) || nextCellText === systemCase.serialNumber) {
+        nextId[1] = Number(nextId[1]) + 1 + ''
+        nextCell = document.querySelector(".serial_number[data-data='" + nextId.join(';') + "']")
+        if (!nextCell) {
+          break
+        }
+        nextCellText = nextCell.innerHTML
+      }
+      if (nextCell) {
+        nextCell.focus()
+      }
+      //TextToSpeech
+      if (voice) {
+        const row = document.querySelector(".serial_number[data-data='" + nextId.join(';') + "']").parentElement
+        const cells = row.children
+
+        if (cells) {
+          for (const cell of cells) {
+            if (cell.className === 'type') {
+              textToSpeech(cell.innerText, 1)
+              break
+            }
+          }
+
+        }
+      }
+    }
+    painting()
   }
 }
--- public/assembly.js
+++ public/assembly.js
@@ -20,17 +20,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.onkeydown = function (e) {
 
-  if (e.ctrlKey && e.key == 'ArrowRight') {
+  if (e.ctrlKey && e.key === 'ArrowRight') {
     e.preventDefault()
     getNextPC()
-  } else if (e.ctrlKey && e.key == 'ArrowLeft') {
+  } else if (e.ctrlKey && e.key === 'ArrowLeft') {
     e.preventDefault()
     getPreviousPC()
   }
 }
 
 function loadPage() {
-  postData('/assembly/serialNumbers')
+  getData('/assembly/serialNumbers')
     .then((data) => {
       let select = document.getElementById("serials")
       select.innerHTML = ''
@@ -42,20 +42,20 @@ function loadPage() {
       }
       document.getElementById('overlay').style.display = 'none'
     })
-  postData('/assembly/firstPC')
+  getData('/assembly/currentSystemCase')
     .then((data) => {
       const select = document.getElementById('serials')
-      select.value = data.firstPC.serial_number
+      select.value = data.currentSystemCase.serialNumber
       document.getElementById('PreviousPC').style.display = 'none'
-      CreateTableFromJSON(data.firstPC, function () {
+      CreateTableFromJSON(data.currentSystemCase, function () {
         painting()
         document.getElementById('overlay').style.display = 'none'
         document.querySelectorAll('[data-obj="1"]')[0].focus()
       })
-      if (data.firstPC.serial_number === data.firstSN) {
+      if (data.currentSystemCase.serialNumber === data.firstSN) {
         document.getElementById('PreviousPC').style.display = 'none'
         document.getElementById('NextPC').style.display = 'inline'
-      } else if (data.firstPC.serial_number == data.lastSN) {
+      } else if (data.currentSystemCase.serialNumber === data.lastSN) {
         document.getElementById('PreviousPC').style.display = 'inline'
         document.getElementById('NextPC').style.display = 'none'
       } else {
@@ -65,22 +65,19 @@ function loadPage() {
     })
 }
 
-function getPC(serialNumberPC, metod) {
-  const data = {
-    serialNumberPC: serialNumberPC
-  }
-  postData('/assembly/getPC', data)
+function getPC(serialNumberSystemCase, metod) {
+  getData(`/assembly/getSystemCase/${serialNumberSystemCase}`)
     .then((data) => {
       CreateTableFromJSON(data, function () {
         painting()
         document.getElementById('overlay').style.display = 'none'
         document.querySelectorAll('[data-obj="1"]')[0].focus()
-        const card = document.querySelector('.pcCardAssembly')
+        const card = document.querySelector('.pcCard')
         const audio = {};
         audio["slide"] = new Audio();
         audio["slide"].src = "/sounds/wave.mp3"
         audio["slide"].play()
-        if (metod == 'next') {
+        if (metod === 'next') {
           card.classList.add('pcCardAssemblyNext')
         } else {
           card.classList.add('pcCardAssemblyPrevious')
@@ -92,107 +89,9 @@ function getPC(serialNumberPC, metod) {
 function CreateTableFromJSON(data, callback) {
   let divContainer = document.getElementById("PC")
   divContainer.innerHTML = ""
-
-  // table = TablePc(data)
-  table = tablePC(data, 'systemCase', true,
-    'type',
-    'name',
-    'quantity',
-    'serial_number'
-  )
-
-  let divCont = document.createElement("div")
-  divCont.id = data._id
-  divCont.className = "pcCardAssembly mb-3"
-  divCont.style.cssText = '-webkit-box-shadow: 0 2px 15px 0' + data.back_color + ';box-shadow: 0 2px 15px 0' + data.back_color
-  divContainer.appendChild(divCont);
-  divCont.innerHTML = ""
-  divCont.appendChild(table)
+  createSystemCaseTable(data, divContainer, true)
   callback()
 }
-
-// function TablePc(pc) {
-//   const apkziDiv = document.getElementById('apkziDiv')
-//   apkziDiv.innerHTML = ''
-//   // таблица ПЭВМ
-//   let table = document.createElement("table");
-//   table.className = "table table-bordered table-hover table-responsive assemblytable"
-//   table.id = pc._id
-
-//   let tr = table.insertRow(-1)
-
-//   td = document.createElement("td")
-//   td.innerHTML = pc.serial_number
-//   td.id = 'serial_number'
-//   td.style.cssText = 'font-size: 1.5rem;border-radius: 4px 0px 0px 0px;background-color:' + pc.back_color
-//   tr.appendChild(td)
-//   insCell('', tr, pc.arm, 'up', '', false)
-//   insCell('', tr, pc.execution, 'up', '', false)
-
-//   td = document.createElement("td")
-//   if (pc.attachment) {
-//     td.innerHTML = pc.attachment
-//     td.style.cssText = 'font-size: 1.1rem;border-radius: 0px 4px 0px 0px;background-color:' + pc.back_color
-//   }
-//   tr.appendChild(td)
-//   if (pc.system_case_unit.length > 0) {
-//     tr = table.insertRow(-1) // TABLE ROW.
-//     insCell('', tr, 'Наименование изделия', 'header', '', false)
-//     insCell('', tr, 'Характеристика', 'header', '', false)
-//     insCell('', tr, 'Количество', 'header', '', false)
-//     insCell('', tr, 'Заводской номер', 'header', '', false)
-//   }
-//   let arr_pc_unit = pc.pc_unit
-//   let arr_system_case_unit = pc.system_case_unit
-//   for (let j = 0; j < arr_pc_unit.length; j++) {
-//     if (arr_pc_unit[j].apkzi && arr_pc_unit[j].serial_number != '') {
-//       const apkziDiv = document.getElementById('apkziDiv')
-//       apkziDiv.innerHTML = 'Номер АПКЗИ - ' + arr_pc_unit[j].serial_number
-//       apkziDiv.style.display = 'block'
-//     }
-//   }
-//   for (let j = 0; j < arr_system_case_unit.length; j++) {
-//     tr = table.insertRow(-1)
-//     insCell('', tr, arr_system_case_unit[j].type, 'type', '', false, {
-//       'id': pc._id
-//     })
-//     insCell('', tr, arr_system_case_unit[j].name, 'name', '', false, {
-//       'id': pc._id
-//     })
-//     insCell('', tr, arr_system_case_unit[j].quantity, '', '', false, {
-//       'id': pc._id
-//     })
-//     let serial_numberCell = tr.insertCell(-1)
-//     serial_numberCell.innerHTML = arr_system_case_unit[j].serial_number
-//     serial_numberCell.dataset.id = pc._id
-//     serial_numberCell.dataset.obj = j
-//     serial_numberCell.dataset.unit = 'system_case_unit'
-//     serial_numberCell.dataset.data = pc._id + ';' + j + ';' + 'system_case_unit'
-//     if (arr_system_case_unit[j].szi) {
-//       serial_numberCell.dataset.apkzi = 'szi'
-//     }
-//     serial_numberCell.className = 'serial_number'
-//     serial_numberCell.contentEditable = "true"
-//     serial_numberCell.addEventListener('keypress', function (e) {
-//       if (e.key == 'Enter') {
-//         e.preventDefault()
-//         const id = e.target.dataset.id
-//         const obj = e.target.dataset.obj
-//         const unit = e.target.dataset.unit
-//         const serial_number = e.target.innerText
-//         const data_hidd = e.target.dataset.data
-//         const data_apkzi = e.target.dataset.apkzi
-//         document.getElementById('hidd_id').value = data_hidd
-//         if (data_apkzi) {
-//           edit_serial_number_apkzi(id, obj, unit, serial_number)
-//         } else {
-//           edit_serial_number(id, obj, unit, serial_number)
-//         }
-//       }
-//     })
-//   }
-//   return table
-// }
 
 function painting() {
   const status = document.getElementById('status')
@@ -212,29 +111,10 @@ function painting() {
   }
 }
 
-function edit_serial_number(id, obj, unit, serialNumber) {
-  serialNumber = translate(serialNumber).ruToEnSN
-  const data = {
-    id: id,
-    obj: obj,
-    unit: unit,
-    serial_number: serialNumber
-  }
-  postData('/pcPa/insert_serial', data).then((data) => {
-    flashAlert(data, data.pc.serial_number)
-    if (!data.duplicatePki) {
-      const serialNumber = data.pc.serial_number
-      const oldNumberMachine = (data.oldNumberMachine !== serialNumber) ? data.oldNumberMachine : null
-      UpdateCells(data.pc, oldNumberMachine)
-      const user = document.getElementById('userName').value
-      const id = data.pc._id
-      socket.emit('updateAssemblyPC', {
-        serialNumber,
-        user,
-        id
-      })
-    }
-  })
+function refreshOneTable(systemCase) {
+  const container = document.getElementById(systemCase._id)
+  container.innerHTML = ""
+  createSystemCaseTable(systemCase, container, true)
 }
 
 function edit_serial_number_apkzi(id, obj, unit, serialNumber) {
@@ -267,7 +147,7 @@ function allOK() {
   const nameCells = document.querySelectorAll('td.name')
   const snCells = document.querySelectorAll('td.serial_number')
   const nameIsOk = ([...nameCells]).reduce((prev, value) => {
-    if (value.innerHTML == 'Н/Д') {
+    if (value.innerHTML === 'Н/Д') {
       value.style.backgroundColor = 'coral'
       prev = false
     }
@@ -283,69 +163,22 @@ function allOK() {
   return (nameIsOk && snIsOk)
 }
 
-function UpdateCells(pc) {
-  let divContainer = document.getElementById(pc._id)
-  divContainer.innerHTML = ""
-  table = tablePC(pc, 'systemCase', true,
-    'type',
-    'name',
-    'quantity',
-    'serial_number'
-  )
-  let divCont = document.createElement("div")
-  divCont.id = pc._id
-  divContainer.className = "tableContent table-responsive"
-  divContainer.appendChild(divCont)
-  divCont.innerHTML = ""
-  divCont.appendChild(table)
-
-  //переход на одну ячейку вниз
-  let current_id = document.getElementById('hidd_id').value
-  let next_id = current_id.split(";")
-  next_id[1] = Number(next_id[1]) + 1 + ''
-  if (!document.querySelector('.popup-checkbox').checked) {
-    let nextCell = document.querySelector(".serial_number[data-data='" + next_id.join(';') + "']")
-    let nextCellText
-    if (nextCell) {
-      nextCellText = nextCell.innerHTML
-      while (/[Бб].?[Нн]/g.test(nextCellText) || nextCellText == pc.serial_number) {
-        next_id[1] = Number(next_id[1]) + 1 + ''
-        nextCell = document.querySelector(".serial_number[data-data='" + next_id.join(';') + "']")
-        if (!nextCell) {
-          break
-        }
-        nextCellText = nextCell.innerHTML
-      }
-      if (nextCell) {
-        nextCell.focus()
-      }
-      //TextToSpeech
-      let rows = document.querySelector(".serial_number[data-data='" + next_id.join(';') + "']").parentElement
-      let row = rows.children
-      if (row) {
-        textToSpeech(row[0].innerText, 1)
-      }
-    }
-    painting()
-  }
-}
-
 function insCell(unit, parrent, html = '', classN, id, contentEditable, dataset) {
   let cell = parrent.insertCell(-1)
   if (classN) cell.className = classN
   if (id) cell.id = id
   if (contentEditable) cell.contentEditable = contentEditable
   cell.innerHTML = html
-  if (id == 'serial_number') {
-    if (unit == 'Системный блок' || unit == 'Сетевой фильтр' || unit == 'Гарнитура' || unit == 'Корпус') {
+  if (id === 'serial_number') {
+    if (unit === 'Системный блок' || unit === 'Сетевой фильтр' || unit === 'Гарнитура' || unit === 'Корпус') {
       cell.className = "serial_number number_mashine"
-    } else if (unit == 'Вентилятор процессора') {
+    } else if (unit === 'Вентилятор процессора') {
       cell.innerHTML = 'б/н'
     } else {
       cell.className = "serial_number"
     }
   }
-  if (id == 'notes' && unit == 'Системный блок') {
+  if (id === 'notes' && unit === 'Системный блок') {
     cell.innerHTML = 'с кабелем питания'
   }
   if (dataset) {
@@ -359,7 +192,7 @@ function nextPC(serialNumber) {
   const select = document.getElementById('serials')
   let nextPCIndex
   for (let i = 0; i < select.length; i++) {
-    if (select[i].value == serialNumber) {
+    if (select[i].value === serialNumber) {
       nextPCIndex = ++i
       break
     }
@@ -371,7 +204,7 @@ function previousPC(serialNumber) {
   const select = document.getElementById('serials')
   let NextPCIndex
   for (let i = 0; i < select.length; i++) {
-    if (select[i].value == serialNumber) {
+    if (select[i].value === serialNumber) {
       NextPCIndex = --i
       break
     }
@@ -390,11 +223,11 @@ function getNextPC() {
   setTimeout(() => {
     nextButton.classList.remove("buttonsActive")
   }, 300);
-  if (lastPCSerialNumber != serialNumber) {
+  if (lastPCSerialNumber !== serialNumber) {
     getPC(serialNumberPlusOne, 'next')
     select.value = serialNumberPlusOne
   }
-  if (serialNumberPlusOne == lastPCSerialNumber) {
+  if (serialNumberPlusOne === lastPCSerialNumber) {
     nextButton.style.display = 'none'
   }
   document.getElementById('PreviousPC').style.display = 'inline'
@@ -411,11 +244,11 @@ function getPreviousPC() {
   setTimeout(() => {
     previousButton.classList.remove("buttonsActive")
   }, 300);
-  if (firstPCSerialNumber != serialNumber) {
+  if (firstPCSerialNumber !== serialNumber) {
     getPC(serialNumberMinusOne, 'previous')
     select.value = serialNumberMinusOne
   }
-  if (serialNumberMinusOne == firstPCSerialNumber) {
+  if (serialNumberMinusOne === firstPCSerialNumber) {
     document.getElementById('PreviousPC').style.display = 'none'
   }
   document.getElementById('NextPC').style.display = 'inline'
